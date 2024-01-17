e0c9f2f7f28b28a239e3e56c584d384377af1916
Broccolito <wanjungu001@gmail.com>
2024-01-17 00:09:38 -0800

    modifying main file

--- example.py
+++ example.py
@@ -14,25 +14,43 @@ def main():
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
        
-    # fasta_parser = FastaParser('./tests/good.fa')
-    # records = list(fasta_parser)
+    fasta_parser = FastaParser('./tests/good.fa')
+    fasta_records = list(fasta_parser)
 
-    # print(records)
+    print("\n\n")
+    print("Printing transcribed fasta sequence...\n")
+    for fasta_sequence in fasta_records:
+        print(transcribe(fasta_sequence[1]))
 
-    # fastq_parser = FastqParser('./tests/good.fq')
-    # records = list(fastq_parser)
-
-    # print(records)
 
     # For each record of FastqParser, Transcribe the sequence
     # and print it to console
 
+    fastq_parser = FastqParser('./tests/good.fq')
+    fastq_records = list(fastq_parser)
+    
+    print("\n\n")
+    print("Printing transcribed fastq sequence...\n")
+    for fastq_sequence in fastq_records:
+        print(transcribe(fastq_sequence[1]))
+
 
     # For each record of FastaParser, Reverse Transcribe the sequence
     # and print it to console
+
+    print("\n\n")
+    print("Printing reverse transcribed fasta sequence...\n")  
+    for fasta_sequence in fasta_records:
+        print(reverse_transcribe(fasta_sequence[1]))
+
        
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
+    
+    print("\n\n")
+    print("Printing reverse transcribed fastq sequence...\n")  
+    for fastq_sequence in fastq_records:
+        print(reverse_transcribe(fastq_sequence[1]))
 
 
 """
